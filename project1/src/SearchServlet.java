bcb71b9de47330c8e687335c013ec398ddd83a6a
mhemedez <mhemedez@uci.edu>
2018-04-23 21:48:21 -0700

    Added total page count in the java servlet

--- project1/src/SearchServlet.java
+++ project1/src/SearchServlet.java
@@ -23,7 +23,6 @@ public class SearchServlet extends HttpServlet {
 	     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	     */
 	    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-	
 	    	
 	    	String title = request.getParameter("title");
 	        String year = request.getParameter("eyear");
@@ -44,7 +43,7 @@ public class SearchServlet extends HttpServlet {
 	        
 	        String loginUser = "mytestuser";
 	        String loginPasswd = "mypassword";
-	        String loginUrl = "jdbc:mysql://localhost/moviedb";
+	        String loginUrl = "jdbc:mysql://localhost/moviedb?allowMultiQueries=true";
 	        
 	        try 
 	        {
@@ -55,10 +54,8 @@ public class SearchServlet extends HttpServlet {
 	    		Statement statement = connection.createStatement( ResultSet.TYPE_SCROLL_INSENSITIVE, 
 	    				   				ResultSet.CONCUR_READ_ONLY);
 	    		// prepare query
-	    		String query ="SELECT m.id, m.title, m.year, m.director, r.rating, r.numVotes, GROUP_CONCAT(DISTINCT s.name SEPARATOR ',') AS stars, GROUP_CONCAT(DISTINCT g.name SEPARATOR ',') AS genres " + 
-	    				"FROM movies AS m, stars AS s, stars_in_movies AS SM, ratings AS r, genres AS g, genres_in_movies AS gm " + 
-	    				"WHERE m.id = SM.movieId AND SM.starId = s.id AND r.movieId = m.id AND g.id = gm.genreId AND gm.movieId = m.id AND "; 
-	 
+	    		String query ="FROM movies AS m, stars AS s, stars_in_movies AS SM, ratings AS r, genres AS g, genres_in_movies AS gm " + 
+	    				"WHERE m.id = SM.movieId AND SM.starId = s.id AND r.movieId = m.id AND g.id = gm.genreId AND gm.movieId = m.id AND ";
 	    		
     			if(title != null && !title.equals("")) 
     				query += "m.title LIKE '%" + title + "%' AND ";
@@ -80,13 +77,27 @@ public class SearchServlet extends HttpServlet {
 	    			query = query.substring(0, query.length() - 4);
 	    		}
 	    		
-	    		query += "GROUP BY m.id "
+	    		String selectQuery1 = "SELECT m.id, m.title, m.year, m.director, r.rating, r.numVotes, GROUP_CONCAT(DISTINCT s.name SEPARATOR ',') AS stars, GROUP_CONCAT(DISTINCT g.name SEPARATOR ',') AS genres " + query;
+	    		String selectQuery2 = "SELECT COUNT(DISTINCT m.id) AS total " + query + ";";
+	    		
+	    		selectQuery1 += "GROUP BY m.id "
 	    				+ "ORDER BY m.year ASC "
 	    				+ "LIMIT " + limit + " "
 	    				+ "OFFSET " + offset + ";";
 	    		
-	    		ResultSet resultSet = statement.executeQuery(query);
+	    		boolean hasResultSets = statement.execute(selectQuery2 + selectQuery1);
+	    		ResultSet resultSet = statement.getResultSet();
 	    		
+	    		int totalResults = 0;
+	    		int totalPages = 0;
+	    		while(resultSet.next())
+	    		{
+	    			totalResults = resultSet.getInt("total");
+	    		}
+	    		totalPages = (int)Math.ceil((1.0 * totalResults)/ (1.0 * Integer.parseInt(limit)));
+	    		
+	    		statement.getMoreResults();
+	    		resultSet = statement.getResultSet();
 	    		
 	    		ArrayList<String> movieTitles = new ArrayList<String>();
 	    		HashMap<String, HashSet<String>> actors = new HashMap<String, HashSet<String>>();
@@ -120,6 +131,7 @@ public class SearchServlet extends HttpServlet {
 	                request.setAttribute("actors", actors);
 	                request.setAttribute("genres", genres);
 	                request.setAttribute("pageNum", pageNum);
+	                request.setAttribute("totalPages", totalPages);
 	                request.setAttribute("status", "Success");
 	    			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/movielist.jsp");
 	                dispatcher.forward(request, response);
