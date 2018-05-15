7c9ee12a31e8a03a4225a6df89512209df9ac4ae
mhemedez <mhemedez@uci.edu>
2018-05-14 17:25:07 -0700

    Updated loginservlet to use preparedstatements and encrypted passwords

--- project1/src/LoginServlet.java
+++ project1/src/LoginServlet.java
@@ -1,5 +1,5 @@
 import com.google.gson.JsonObject;
-
+import org.jasypt.util.password.StrongPasswordEncryptor;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,18 +8,16 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-//
+
 @WebServlet(name = "LoginServlet", urlPatterns = "/api/login")
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
+    
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     	String gRecaptchaResponse = request.getParameter("g-recaptcha-response");
@@ -40,69 +38,74 @@ public class LoginServlet extends HttpServlet {
             return;
         }
     	
-    	
-    	
         /* This example only allows username/password to be test/test
         /  in the real project, you should talk to the database to verify username/password
         */
         String loginUser = "mytestuser";
         String loginPasswd = "mypassword";
+        String loginUrl = "jdbc:mysql://localhost:3306/moviedb";
 
-        String loginUrl = "jdbc:mysql://18.188.117.207:3306/moviedb";
-
-        
         // get the printwriter for writing response
         PrintWriter out = response.getWriter();
 
-        
-        
         try {
     		Class.forName("com.mysql.jdbc.Driver").newInstance();
     		// create database connection
     		Connection connection = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);
-    		// declare statement
-    		Statement statement = connection.createStatement( ResultSet.TYPE_SCROLL_INSENSITIVE, 
-    				   				ResultSet.CONCUR_READ_ONLY);
+    		
+    		// Create Query
+    		String query = "SELECT c.firstName, c.lastName, c.id, c.password FROM customers AS c WHERE c.email LIKE ? OR c.email LIKE ?;";
+    		
+    		//Create Statement Connection
+	        PreparedStatement statement = connection.prepareStatement(query);
+    		
     		// get the user input for the query
     		String username = request.getParameter("username");
 	        String password = request.getParameter("password");
 	        
+	        //Modify Prepared Statement
+	        statement.setString(1, username);
+	        username += "@%";
+	        statement.setString(2, username);
 	        
-
-	        String query = "SELECT c.firstName, c.lastName, c.id " + 
-    				"FROM customers AS c " + 
-    				"WHERE c.password LIKE '" + password + "' AND (c.email LIKE '"+ username + "' OR c.email LIKE '" + username +"@%');";
-    		 
     		// execute query
-    		ResultSet resultSet = statement.executeQuery(query);
-    		
+    		ResultSet resultSet = statement.executeQuery();
+    		boolean success = false;
     		if (resultSet.next()) {
                 // Login success:
                 // set this user into the session
-    			String id = resultSet.getString("id");
-                request.getSession().setAttribute("user", new User(username, id));
+    			String encryptedPassword = resultSet.getString("password");
+    			success = new StrongPasswordEncryptor().checkPassword(password, encryptedPassword);
+    			if(success == true)
+    			{
+    				String id = resultSet.getString("id");
+                    request.getSession().setAttribute("user", new User(username, id));
 
-                JsonObject responseJsonObject = new JsonObject();
-                responseJsonObject.addProperty("status", "success");
-                responseJsonObject.addProperty("message", "success");
-                responseJsonObject.addProperty("recaptcha", "success");
+                    JsonObject responseJsonObject = new JsonObject();
+                    responseJsonObject.addProperty("status", "success");
+                    responseJsonObject.addProperty("message", "success");
+                    responseJsonObject.addProperty("recaptcha", "success");
 
-                response.getWriter().write(responseJsonObject.toString());
+                    response.getWriter().write(responseJsonObject.toString());
+    			}
+    			else
+    			{
+    				// Password Is Incorrect
+                    JsonObject responseJsonObject = new JsonObject();
+                    responseJsonObject.addProperty("status", "fail");                
+                    responseJsonObject.addProperty("message", "Entered incorrect password, please try again");
+                    response.getWriter().write(responseJsonObject.toString());
+    			}
             } else {
-                // Login fail
+                // Username Not Found
                 JsonObject responseJsonObject = new JsonObject();
-                responseJsonObject.addProperty("status", "fail");
-                
-                responseJsonObject.addProperty("message", "The username password combination doesn't exist, please try again");
-
-
+                responseJsonObject.addProperty("status", "fail");                
+                responseJsonObject.addProperty("message", "The username doesn't exist, please try again");
                 response.getWriter().write(responseJsonObject.toString());
             }
     		resultSet.close();
     		statement.close();
     		connection.close();
-    		
-
     		
         } catch (Exception e) {
     		/*
@@ -114,13 +117,11 @@ public class LoginServlet extends HttpServlet {
     		 *   tail -100 catalina.out
     		 * This can help you debug your program after deploying it on AWS.
     		 */
+        	JsonObject responseJsonObject = new JsonObject();
+            responseJsonObject.addProperty("status", "fail");                
+            responseJsonObject.addProperty("message", "The server is experience some difficulties. Please try again another time.");
+            response.getWriter().write(responseJsonObject.toString());
     		e.printStackTrace();
-    		
-    		out.println("<body>");
-    		out.println("<p>");
-    		out.println("Exception in doGet: " + e.getMessage());
-    		out.println("</p>");
-    		out.print("</body>");
     	}
     
         
