d1f992047e290f262bfffcd8bd464f7560e2682f
mhemedez <mhemedez@uci.edu>
2018-05-15 19:27:21 -0700

    Added new procedures for dashboard

--- project1/create_procedures.sql
+++ project1/create_procedures.sql
@@ -1,8 +1,5 @@
 USE moviedb;
 
-# 
-
-
 # Checkout procedure
 DROP PROCEDURE IF EXISTS checkout;
 
@@ -41,5 +38,83 @@ BEGIN
 			SELECT 'SUCCESS' AS status, '' AS message;
 		END IF;
 		CLOSE cur;
+    END IF;
+END$$
+
+# add_movie procedure
+DROP PROCEDURE IF EXISTS add_movie;
+
+DELIMITER $$
+CREATE PROCEDURE add_movie (IN mov_title VARCHAR(100), IN mov_year int(11), 
+	IN mov_director VARCHAR(100), IN mov_star_name VARCHAR(100), IN mov_genre_name VARCHAR(32))
+COMMENT "Adds a new movie into the movie table and adds star and genre to their respective tables"
+
+BEGIN
+	DECLARE mov_id VARCHAR(10);
+    DECLARE mov_exists CHAR(1);
+    DECLARE genre_id CHAR(10);
+    DECLARE genre_exists CHAR(1);
+    DECLARE star_id CHAR(10);
+    DECLARE star_exists CHAR (1);
+    DECLARE status_message VARCHAR(1000);
+    
+    SET status_message = '';
+    
+    SELECT CONCAT("tt", SUBSTRING(id, 3, CHAR_LENGTH(id) - 2 -CHAR_LENGTH(SUBSTRING(id, 3, CHAR_LENGTH(id) - 2) + 1)), SUBSTRING(id, 3, CHAR_LENGTH(id) - 2) + 1)
+		INTO mov_id FROM movies ORDER BY id DESC LIMIT 1;
+    SELECT DISTINCT 't' INTO mov_exists FROM movies AS m WHERE m.title = mov_title AND m.year = mov_year AND m.director = mov_director;
+    SELECT DISTINCT 't' INTO genre_exists FROM genres AS g WHERE g.name = mov_genre_name;
+    SELECT DISTINCT 't' INTO star_exists FROM stars AS s WHERE s.name = mov_star_name;
+    
+    IF (mov_exists = 't') THEN
+		SELECT 'ERROR' AS status, 'Movie not added to the database because the movie already exists.' AS message;
+	ELSE
+		# Insert into movies table
+		INSERT INTO movies (id, title, year, director) VALUES (mov_id, mov_title, mov_year, mov_director);
+        SET status_message = CONCAT(status_message, 'New Movie was added to movies table. ');
+        
+        # Insert into genres and genres_in_movies tables
+        IF (genre_exists IS NULL) THEN
+			INSERT INTO genres (name) VALUES (mov_genre_name);
+            SET status_message = CONCAT(status_message, 'New Genre was added to genres table. ');
+		END IF;
+		SELECT id INTO genre_id FROM genres AS g WHERE g.name LIKE mov_genre_name LIMIT 1;
+		INSERT INTO genres_in_movies (genreId, movieId) VALUES (genre_id, mov_id);
+        SET status_message = CONCAT(status_message, 'Linked movie to genre in genres_in_movies table. ');
+        
+        # Insert into stars and stars_in_movies tables
+		IF (star_exists IS NULL) THEN
+			SELECT CONCAT("nm", SUBSTRING(id, 3, CHAR_LENGTH(id) - 2 -CHAR_LENGTH(SUBSTRING(id, 3, CHAR_LENGTH(id) - 2) + 1)), SUBSTRING(id, 3, CHAR_LENGTH(id) - 2) + 1) INTO star_id FROM stars ORDER BY id DESC LIMIT 1;
+			INSERT INTO stars (id, name, birthYear) VALUES (star_id, mov_star_name, NULL);
+            SET status_message = CONCAT(status_message, 'New Star was added to stars table. ');
+		END IF;
+		SELECT id INTO star_id FROM stars AS s WHERE s.name LIKE mov_star_name ORDER BY s.birthYear LIMIT 1;
+		INSERT INTO stars_in_movies (starId, movieId) VALUES (star_id, mov_id);
+        SET status_message = CONCAT(status_message, 'Linked star to movie in stars_in_movies table.');
+        
+        SELECT 'SUCCESS' AS status, RTRIM(status_message) AS message;
+    END IF;
+END$$
+
+# add_star procedure
+DROP PROCEDURE IF EXISTS add_star;
+
+DELIMITER $$
+CREATE PROCEDURE add_star (IN star_name VARCHAR(100), IN star_birth_year int(11))
+COMMENT "Allows an employee to add a star into the system"
+
+BEGIN
+	DECLARE star_exists CHAR(1);
+    DECLARE star_id VARCHAR(10);
+    
+    SELECT CONCAT("nm", SUBSTRING(id, 3, CHAR_LENGTH(id) - 2 -CHAR_LENGTH(SUBSTRING(id, 3, CHAR_LENGTH(id) - 2) + 1)), SUBSTRING(id, 3, CHAR_LENGTH(id) - 2) + 1) INTO star_id FROM stars ORDER BY id DESC LIMIT 1;
+    SELECT DISTINCT 't' INTO star_exists FROM stars AS s WHERE s.name = star_name AND s.birthYear = star_birth_year;
+    
+    INSERT INTO stars (id, name, birthYear) VALUES (star_id, star_name, star_birth_year);
+    
+	IF (star_exists LIKE 't') THEN
+        SELECT 'SUCCESS' AS status, 'Star added to database. Possibly added duplicate star.' AS message;
+	ELSE
+		SELECT 'SUCCESS' AS status, 'Star added to database.' AS message;
     END IF;
 END$$
