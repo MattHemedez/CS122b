822a48ab814269d7c7641c124f45f48d15fc2cf1
mhemedez <mhemedez@uci.edu>
2018-05-23 16:59:36 -0700

    Added browser caching of suggestions results

--- project1/WebContent/autocomplete.js
+++ project1/WebContent/autocomplete.js
@@ -1,6 +1,13 @@
+// Initial Variables
+var localDict = {};
 
-
-
+var result = {
+        suggestions: [
+            { "value": "United Arab Emirates", "data": "AE" },
+            { "value": "United Kingdom",       "data": "UK" },
+            { "value": "United States",        "data": "US" }
+        ]
+    };
 /*
  * This function is called by the library when it needs to lookup a query.
  * 
@@ -9,27 +16,37 @@
  *   suggestion list from AJAX, you need to call this function to let the library know.
  */
 function handleLookup(query, doneCallback) {
-	console.log("autocomplete initiated")
-	console.log("sending AJAX request to backend Java Servlet")
+	console.log("autocomplete initiated");
 	
-	// TODO: if you want to check past query results first, you can do it here
-	
-	// sending the HTTP GET request to the Java Servlet endpoint hero-suggestion
-	// with the query data
-	jQuery.ajax({
-		"method": "GET",
-		// generate the request url from the query.
-		// escape the query string to avoid errors caused by special characters 
-		"url": "hero-suggestion?query=" + escape(query),
-		"success": function(data) {
-			// pass the data, query, and doneCallback function into the success handler
-			handleLookupAjaxSuccess(data, query, doneCallback) 
-		},
-		"error": function(errorData) {
-			console.log("lookup ajax error")
-			console.log(errorData)
-		}
-	})
+	console.log("looking up in local cache");
+	// checking local cache of suggestions
+	if(localDict.hasOwnProperty(escape(query)))
+	{
+		console.log("found suggestions in local cache");
+		var result = { suggestions: localDict[escape(query)] };
+		console.log(result);
+		doneCallback(result);
+	}
+	// sending the HTTP GET request to the Java Servlet with the query data
+	else
+	{
+		console.log("not in local cache");
+		console.log("sending AJAX request to backend Java Servlet");
+		jQuery.ajax({
+			"method": "GET",
+			// generate the request url from the query.
+			// escape the query string to avoid errors caused by special characters 
+			"url": "movie-suggestion?query=" + escape(query),
+			"success": function(data) {
+				// pass the data, query, and doneCallback function into the success handler
+				handleLookupAjaxSuccess(data, query, doneCallback) 
+			},
+			"error": function(errorData) {
+				console.log("lookup ajax error")
+				console.log(errorData)
+			}
+		})
+	}
 }
 
 
@@ -47,8 +64,9 @@ function handleLookupAjaxSuccess(data, query, doneCallback) {
 	var jsonData = JSON.parse(data);
 	console.log(jsonData)
 	
-	// TODO: if you want to cache the result into a global variable you can do it here
-
+	// result into a global variable
+	localDict[escape(query)] = data;
+	
 	// call the callback function provided by the autocomplete library
 	// add "{suggestions: jsonData}" to satisfy the library response format according to
 	//   the "Response Format" section in documentation
@@ -71,33 +89,26 @@ function handleSelectSuggestion(suggestion) {
 }
 
 
-/*
- * This statement binds the autocomplete library with the input box element and 
- *   sets necessary parameters of the library.
- * 
- * The library documentation can be find here: 
- *   https://github.com/devbridge/jQuery-Autocomplete
- *   https://www.devbridge.com/sourcery/components/jquery-autocomplete/
- * 
- */
 // $('#autocomplete') is to find element by the ID "autocomplete"
-$('#autocomplete').autocomplete({
+$('#autocomplete').autocomplete(
+{
 	// documentation of the lookup function can be found under the "Custom lookup function" section
     lookup: function (query, doneCallback) {
-		if(query.length >= 3)
-		{
-			handleLookup(query, doneCallback)
-		}
+		handleLookup(query, doneCallback)
     },
     onSelect: function(suggestion) {
     		handleSelectSuggestion(suggestion)
     },
     // set the groupby name in the response json data field
-    groupBy: "genre",
+    groupBy: "title",
     // set delay time
     deferRequestBy: 300,
-    // there are some other parameters that you might want to use to satisfy all the requirements
-    
+    // prevents queries starting with problematic first words to stop generating queries
+    preventBadQueries: true,
+    // set minimum characters needed before an autocomplete query gets sent
+    minChars: 3,
+    // set maximum number of results listed from local lookup
+    lookupLimit: 10,
     // TODO: add other parameters, such as minimum characters
 });
 
